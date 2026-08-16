5394e222428f5f83751745a3502305fcde7d04e4
York <yorkwuo@gmail.com>
2026-08-16 16:24:42 +0800

    n

---
+++ projs/earlgrey_sky130/ddm/chip_earlgrey_asic/rtl/v1/chip_earlgrey_asic_bbox.sv
@@ -0,0 +1,181 @@
+(* blackbox *)
+module ast (
+  // tlul if
+  input tlul_pkg::tl_h2d_t tl_i,              // TLUL H2D
+  output tlul_pkg::tl_d2h_t tl_o,             // TLUL D2H
+  output prim_mubi_pkg::mubi4_t ast_init_done_o,  // AST (registers) Init Done
+
+  // clocks / resets
+  input clk_ast_adc_i,                        // Buffered AST ADC Clock
+  input rst_ast_adc_ni,                       // Buffered AST ADC Reset
+  input clk_ast_alert_i,                      // Buffered AST Alert Clock
+  input rst_ast_alert_ni,                     // Buffered AST Alert Reset
+  input clk_ast_es_i,                         // Buffered AST Entropy Source Clock
+  input rst_ast_es_ni,                        // Buffered AST Entropy Source Reset
+  input clk_ast_rng_i,                        // Buffered AST RNG Clock
+  input rst_ast_rng_ni,                       // Buffered AST RNG Reset
+  input clk_ast_tlul_i,                       // Buffered AST TLUL Clock
+  input rst_ast_tlul_ni,                      // Buffered AST TLUL Reset
+  input clk_ast_usb_i,                        // Buffered AST USB Clock
+  input rst_ast_usb_ni,                       // Buffered AST USB Reset
+  input clk_ast_ext_i,                        // Buffered AST External Clock
+  input por_ni,                               // Power ON Reset
+
+  // sensed clocks / resets
+  input clkmgr_pkg::clkmgr_out_t sns_clks_i,  // Sensed Clocks
+  input rstmgr_pkg::rstmgr_out_t sns_rsts_i,  // Sensed Resets
+  input sns_spi_ext_clk_i,                    // Sensed SPI External Clock
+`ifdef AST_BYPASS_CLK
+  // Clocks' Oschillator bypass for OS FPGA
+  input ast_pkg::clks_osc_byp_t clk_osc_byp_i,  // Clocks' Oschillator bypass for OS FPGA/VERILATOR
+`endif
+  // power OK control
+  // In non-power aware DV environment, the <>_supp_i is for debug only!
+  // POK signal follow this input.
+  // In a power aware environment this signal should be connected to constant '1'
+  input vcc_supp_i,                           // VCC Supply Test for OS FPGA
+  input vcaon_supp_i,                         // VCAON Supply Test for OS FPGA
+  input vcmain_supp_i,                        // VCMAIN Supply Test for OS FPGA
+  input vioa_supp_i,                          // VIOA Rail Supply Test for OS FPGA
+  input viob_supp_i,                          // VIOB Rail Supply Test for OS FPGA
+  output ast_pkg::ast_pwst_t ast_pwst_o,      // AON, MAIN, IO-0 Rail, IO-1 Rail Power OK @1.1V
+  output ast_pkg::ast_pwst_t ast_pwst_h_o,    // AON, MAIN, IO-9 Rail, IO-1 Rail Power OK @3.3V
+
+  // Power and IO pin connections
+  input main_pd_ni,                           // MAIN Regulator Power Down
+  input main_env_iso_en_i,                    // Enveloped ISOlation ENable for MAIN
+
+  // power down monitor logic - flash/otp related
+  output logic flash_power_down_h_o,          // Flash Power Down
+  output logic flash_power_ready_h_o,         // Flash Power Ready
+  input [1:0] otp_power_seq_i,                // MMR0,24 in (VDD)
+  output logic [1:0] otp_power_seq_h_o,       // MMR0,24 masked by PDM, out (VCC)
+
+  // system source clock
+  input clk_src_sys_en_i,                     // SYS Source Clock Enable
+  input prim_mubi_pkg::mubi4_t clk_src_sys_jen_i,  // SYS Source Clock Jitter Enable
+  output logic clk_src_sys_o,                 // SYS Source Clock
+  output logic clk_src_sys_val_o,             // SYS Source Clock Valid
+
+  // aon source clock
+  output logic clk_src_aon_o,                 // AON Source Clock
+  output logic clk_src_aon_val_o,             // AON Source Clock Valid
+
+  // io source clock
+  input clk_src_io_en_i,                      // IO Source Clock Enable
+  output logic clk_src_io_o,                  // IO Source Clock
+  output logic clk_src_io_val_o,              // IO Source Clock Valid
+  output prim_mubi_pkg::mubi4_t clk_src_io_48m_o,  // IO Source Clock is 48MHz
+
+  // usb source clock
+  input usb_ref_pulse_i,                      // USB Reference Pulse
+  input usb_ref_val_i,                        // USB Reference Valid
+  input clk_src_usb_en_i,                     // USB Source Clock Enable
+  output logic clk_src_usb_o,                 // USB Source Clock
+  output logic clk_src_usb_val_o,             // USB Source Clock Valid
+  output logic [ast_pkg::UsbCalibWidth-1:0] usb_io_pu_cal_o,  // USB IO Pull-up Calibration Setting
+
+  // adc interface
+  input adc_pd_i,                             // ADC Power Down
+  input ast_pkg::awire_t adc_a0_ai,           // ADC A0 Analog Input
+  input ast_pkg::awire_t adc_a1_ai,           // ADC A1 Analog Input
+  input [ast_pkg::AdcChannels-1:0] adc_chnsel_i,       // ADC Channel Select
+  output [ast_pkg::AdcDataWidth-1:0] adc_d_o,          // ADC Digital (per channel)
+  output adc_d_val_o,                         // ADC Digital Valid
+
+  // rng (entropy source) interface
+  input rng_en_i,                             // RNG Enable
+  input rng_fips_i,                           // RNG FIPS
+  output logic rng_val_o,                     // RNG Valid
+  output logic [ast_pkg::EntropyStreams-1:0] rng_b_o,  // RNG Bit(s)
+
+  // entropy distribution interface
+  input edn_pkg::edn_rsp_t entropy_rsp_i,     // Entropy Response
+  output edn_pkg::edn_req_t entropy_req_o,    // Entropy Request
+
+  // alerts
+  input ast_pkg::ast_alert_rsp_t alert_rsp_i,  // Alerts Trigger & Acknowledge Inputs
+  output ast_pkg::ast_alert_req_t alert_req_o, // Alerts Output
+
+  // dft interface
+  input pinmux_pkg::dft_strap_test_req_t dft_strap_test_i,  // DFT Straps
+  input lc_ctrl_pkg::lc_tx_t lc_dft_en_i,     // DFT enable (secure bus)
+  input [8-1:0] fla_obs_i,                    // FLASH Observe Bus
+  input [8-1:0] otp_obs_i,                    // OTP Observe Bus
+  input [8-1:0] otm_obs_i,                    // OT Modules Observe Bus
+  input usb_obs_i,                            // USB DIFF RX Observe
+  output ast_pkg::ast_obs_ctrl_t obs_ctrl_o,  // Observe Control
+
+  // pad mux/pad related
+  input [ast_pkg::Pad2AstInWidth-1:0] padmux2ast_i,    // IO_2_DFT Input Signals
+  output logic [ast_pkg::Ast2PadOutWidth-1:0] ast2padmux_o,  // DFT_2_IO Output Signals
+  output logic [4-1:0] mux_iob_sel_o, // iob or spi selector
+
+  // analog test outputs
+`ifdef ANALOGSIM
+  output real ast2pad_t0_ao,                  // AST_2_PAD Analog T0 Output Signal
+  output real ast2pad_t1_ao,                  // AST_2_PAD Analog T1 Output Signal
+`else
+  output wire ast2pad_t0_ao,                  // AST_2_PAD Analog T0 Output Signal
+  output wire ast2pad_t1_ao,                  // AST_2_PAD Analog T1 Output Signal
+`endif
+
+  // flash and external clocks
+  input prim_mubi_pkg::mubi4_t ext_freq_is_96m_i,   // External clock frequency is 96MHz
+  input prim_mubi_pkg::mubi4_t all_clk_byp_req_i,   // All clocks bypass request
+  output prim_mubi_pkg::mubi4_t all_clk_byp_ack_o,  // Switch all clocks to External clocks
+  input prim_mubi_pkg::mubi4_t io_clk_byp_req_i,    // IO clock bypass request (for OTP bootstrap)
+  output prim_mubi_pkg::mubi4_t io_clk_byp_ack_o,   // Switch IO clock to External clockn
+  output prim_mubi_pkg::mubi4_t flash_bist_en_o,    // Flush BIST (TAP) Enable
+
+  // Memory configuration request/response interfaces
+  output ast_pkg::ast_mem_cfg_req_t mem_cfg_req_o,
+  input  ast_pkg::ast_mem_cfg_rsp_t mem_cfg_rsp_i,
+
+  // Scan interface
+  output prim_mubi_pkg::mubi4_t dft_scan_md_o,  // Scan Mode output
+  output scan_shift_en_o,                       // Scan Shift Enable output
+  output scan_reset_no                          // Scan Reset output
+);
+endmodule
+
+(* blackbox *)
+module prim_usb_diff_rx #(
+  parameter int CalibW = 32
+) (
+  inout              input_pi,      // differential input
+  inout              input_ni,      // differential input
+  input              input_en_i,    // input buffer enable
+  input              core_pok_h_i,  // core power indication at VCC level
+  input              pullup_p_en_i, // pullup enable for P
+  input              pullup_n_en_i, // pullup enable for N
+  input [CalibW-1:0] calibration_i, // calibration input
+  output logic       usb_diff_rx_obs_o, // observability output
+  output logic       input_o        // output of differential input buffer
+);
+endmodule
+
+(* blackbox *)
+module prim_pad_wrapper
+  import prim_pad_wrapper_pkg::*;
+#(
+  // These parameters are ignored in this model.
+  parameter pad_type_e PadType = BidirStd,
+  parameter pad_orient_e PadOrient = OrientH,
+  parameter scan_role_e ScanRole = NoScan
+) (
+  // This is only used for scanmode (not used in generic models)
+  input              clk_scan_i,
+  input              scanmode_i,
+  // Power sequencing signals (not used in generic models)
+  input pad_pok_t    pok_i,
+  // Main Pad signals
+  inout wire         inout_io, // bidirectional pad
+  output logic       in_o,     // input data
+  output logic       in_raw_o, // uninverted output data
+  input              ie_i,     // input enable
+  input              out_i,    // output data
+  input              oe_i,     // output enable
+  input pad_attr_t   attr_i    // additional pad attributes
+);
+endmodule
