3feb5a475566a10423ec57089e8ad0bf2fc6cbb3
York <yorkwuo@gmail.com>
2026-08-16 14:41:40 +0800

    n

---
+++ projs/earlgrey_sky130/.vars.tcl
@@ -0,0 +1,4 @@
+set vars(depth)                              "0"
+set vars(g2module)                           "proj"
+set vars(pagename)                           "earlgrey_sky130"
+set vars(view)                               "Overview"
